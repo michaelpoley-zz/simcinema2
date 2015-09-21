c3967d637c3150fd9601355a654da17c437d0447
John Landis <johnalandis@gmail.com>
2015-09-21 19:34:54 -0400

    created a function to switch modes

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,25 @@
 
+
 // Hides the game element
 
 $("#game").hide();
 
 money = "Budget: $" + 10000000;
 prestige  = "Hollywood Outsider";
+
+//Selects which mode we are currently in
+function change_mode(new_mode){
+	if($("#" + new_mode).attr('class') != 'mode'){ //we tried to switch to a mode that doesn't have a div in index.html
+		console.log("Tried to switch to mode '" + new_mode + "', which is not a div that I know of.")
+		return null;
+	}
+	else{
+		//hide all the mode divs
+		var mode_divs = $(".mode").hide();
+		//show just the div we want
+		$("#" + new_mode).show()
+		}
+	}
 
 // Prints the name of the Studio from a prompt
 
