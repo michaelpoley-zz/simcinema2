b823ea2f178d22e5fd68cd1d882473d6b19fa8b7
John Landis <johnalandis@gmail.com>
2015-09-28 15:49:58 -0400

    added talentlist.js

---
+++ javascript/talentlist.js
@@ -0,0 +1,8 @@
+TalentList = function(){  //this object should be extended through prototype inheritance to create the director list, actor list, etc.
+	this.list = [];
+	
+}
+
+TalentList.prototype.add(item){
+	this.list.push(item);
+}
