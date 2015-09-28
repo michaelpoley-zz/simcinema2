1a69d173972880917c870109d06d864154db8756
John Landis <johnalandis@gmail.com>
2015-09-27 20:28:12 -0400

    Got studio object's toHTML function working

--- javascript/studio.js
+++ javascript/studio.js
@@ -6,13 +6,26 @@ Studio = function(name, riskiness, generosity){
 	this.generosity = generosity; //how much cash the studio is likely to fork over
 }
 
-Studio.prototype.toHTML = function(){
-	html = "<li>"+
-		this.name + '	' +
-		'<button class= "pitchButton" id="pitchTo' + this.name + '">'
-		+"Pitch Movie to " + this.name +
-		"</button></li>";
+Studio.prototype.pitch = function(prodco, movie){
+	alert("You want to pitch " + prodco.name +"'s " + movie.title + " to " + this.name);
+}
+
+Studio.prototype.toHTML = function(){  //Really not sure if this is done right, but it works
+
+	var myLi = document.createElement("LI"); //create list item
+	myLi.appendChild(document.createTextNode(this.name)); //add name of studio
+	var myButton = document.createElement("BUTTON"); //create a button
+	myButton.appendChild(document.createTextNode("pitch to " +this.name));//give the button some text
+	myLi.appendChild(myButton);// put the button into the list item
+	
+	myButton.onclick = pitchClickHandler; //give the button a function to call when clicked
+	myButton.studioInstance = this; //attach a reference to the instance of this object to the button 
+	
+	html = myLi
 	return html;
 }
 
+pitchClickHandler = function() {
+	this.studioInstance.pitch(currentProdco, currentMovie); //this refers to the button in this case
+}
 
