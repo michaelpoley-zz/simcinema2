ca49f3f8b5dcdf46f48780e5543ef32e854f523e
John Landis <johnalandis@gmail.com>
2015-09-27 21:38:36 -0400

    fixed randInt function

--- javascript/util.js
+++ javascript/util.js
@@ -1,4 +1,4 @@
 //random number generators and other utilities
 function randInt(min,max){//generates a random ingeger between min and max
-	return Math.random() * (max - min) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
